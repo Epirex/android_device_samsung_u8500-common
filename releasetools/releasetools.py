a75ed1800fe7e376283815d32e1f3601571f92ff
Epirex <estebanguzzo@gmail.com>
2017-10-16 20:19:19 -0300

    Revert "releasetools: Symlink libcscall.so deps"
    This reverts commit 87e681ea25dc89f1731b13bb351949a2a088ba01.

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,9 +1,3 @@
 def FullOTA_InstallEnd(info):
 	info.script.AppendExtra('symlink("/system/lib/libjhead.so", "/system/lib/libhead.so");')
 	info.script.AppendExtra('run_program("/sbin/make_ext4fs", "/dev/block/mmcblk0p9");')
-
-# Symlink some dependencies of libcscall.so
-	info.script.AppendExtra('symlink("/system/lib/ste_omxcomponents/libste_dec_amr.so", "/system/lib/libste_dec_amr.so");')
-	info.script.AppendExtra('symlink("/system/lib/ste_omxcomponents/libste_enc_amr.so", "/system/lib/libste_enc_amr.so");')
-	info.script.AppendExtra('symlink("/system/lib/ste_omxcomponents/libste_dec_amrwb.so", "/system/lib/libste_dec_amrwb.so");')
-info.script.AppendExtra('symlink("/system/lib/ste_omxcomponents/libste_enc_amrwb.so", "/system/lib/libste_enc_amrwb.so");')
